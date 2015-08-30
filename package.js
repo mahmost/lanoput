280c944e2dbf29359bd4a725afa6ed9eac300978
Mahmoud Mostafa <mah@moud.info>
2015-08-30 03:18:17 +0200

    Update meteor package file

--- package.js
+++ package.js
@@ -1,12 +1,11 @@
 Package.describe({
   name: "mahmost:lanoput",
-  //git: "https://github.com/mahmost/lanoput.git",
+  git: "https://github.com/mahmost/lanoput.git",
   summary: "Change input language of input/textarea automatically to comply with the element lang attribute",
-  "version": "0.1.0"
+  "version": "0.2.1"
 });
 Package.onUse(function (api) {
   api.versionsFrom('0.9.0');
-  //api.use('jquery', 'client');
   api.addFiles('lanoput.js', 'client');
   api.addFiles('lanoput.ar.js', 'client');
   api.addFiles('lanoput.fa.js', 'client');
