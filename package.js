a59a323a501ef665fb86f65f28a2e598bbe62f9f
Mahmoud Mostafa <mah@moud.info>
2020-03-07 14:35:12 +0200

    Version bump

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: "mahmost:lanoput",
   git: "https://github.com/mahmost/lanoput.git",
   summary: "Change input language of input/textarea automatically to comply with the element lang attribute",
-  "version": "0.2.2"
+  "version": "0.2.3"
 });
 Package.onUse(function (api) {
   api.versionsFrom('0.9.0');
